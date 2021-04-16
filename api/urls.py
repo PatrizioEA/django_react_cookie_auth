21520a739a0a090ffa7e984efc03f53e126589ef
Patrizio Gasperi <patrizio.gasperi@planstudio.it>
2021-04-16 14:16:50 +0200

    First commit

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('csrf/', views.get_csrf, name='api-csrf'),
+    path('login/', views.login_view, name='api-login'),
+    path('logout/', views.logout_view, name='api-logout'),
+    path('session/', views.session_view, name='api-session'),
+    path('whoami/', views.whoami_view, name='api-whoami'),
+    #path('session/', views.SessionView.as_view(), name='api-session'),  # new
+    #path('whoami/', views.WhoAmIView.as_view(), name='api-whoami'),  # new
+]
